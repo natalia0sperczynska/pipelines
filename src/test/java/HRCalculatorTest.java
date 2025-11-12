86a7ff71e7f0a67a4cb81dcb9e8ccff53d61071b
nsperczynska <sperczynskanatalia@gmail.com>
2025-11-12 10:30:14 +0100

    Extended testing suite, task 2

--- src/test/java/HRCalculatorTest.java
+++ src/test/java/HRCalculatorTest.java
@@ -1,9 +1,24 @@
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 public class HRCalculatorTest {
     @Test
-    public void testValidMaxHR(){
+    public void testValidMaxHR() {
         var result = HRCalculator.calculateMaxHR(25);
         assertEquals(195, result);
+    }
+
+    @Test
+    public void testInvalidMinHR_ageTooHigh() {
+        assertThrows(IllegalArgumentException.class,
+                () -> HRCalculator.calculateMaxHR(500));
+    }
+
+    @Test
+    public void testInvalidMinHR_ageTooLow() {
+        assertThrows(IllegalArgumentException.class,
+                () -> HRCalculator.calculateMaxHR(-1));
     }
 }
